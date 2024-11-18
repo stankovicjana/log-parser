3e51d782ce004e2ff33bf0b15f2616585ab6743e
stankovicjana <105547442+stankovicjana@users.noreply.github.com>
2024-11-18 17:38:23 +0100

    Front-end fixed

--- src/sample/views/upload.clj
+++ src/sample/views/upload.clj
@@ -20,7 +20,8 @@
      [:div {:class "content"}
       [:h2 "Upload your file here:"]
       [:form {:id "upload-form" :action "/uploadFile" :method "post" :enctype "multipart/form-data"}
-       [:input {:type "file" :name "file" :id "file"}]
+       [:label {:for "file-upload" :class "custom-file-upload"} "Choose File"]
+       [:input {:id "file-upload" :type "file" :name "file"}]
        [:button {:type "submit"} "Upload file"]]
       [:div {:id "result"}]
       [:h3 "Log Data"]
@@ -31,26 +32,26 @@
          [:th "Owner"]
          [:th "Message"]]]
        [:tbody {:id "log-table-body"}]]]]
-
     [:script
      "document.getElementById('upload-form').onsubmit = function(event) {
         event.preventDefault();
-
         var formData = new FormData(document.getElementById('upload-form'));
-
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/uploadFile', true);
-
         xhr.onload = function () {
           if (xhr.status === 200) {
             try {
               var data = JSON.parse(xhr.responseText);  
+              console.log(data);
               var tableBody = document.getElementById('log-table-body');
               tableBody.innerHTML = '';  
-
               if (data.length > 0) {
                 data.forEach(function(log) {
                   var row = document.createElement('tr');
+                  if (log.error) {  
+                    row.style.backgroundColor = 'red';
+                  }
+
                   row.innerHTML = '<td>' + log.timestamp + '</td><td>' + log.owner + '</td><td>' + log.message + '</td>';
                   tableBody.appendChild(row);
                 });
@@ -63,7 +64,6 @@
                     }
                   });
                 });
-
               } else {
                 var row = document.createElement('tr');
                 row.innerHTML = '<td colspan=\"3\">No data available</td>';
@@ -76,6 +76,5 @@
             alert('An error occurred while uploading the file.');
           }
         };
-
         xhr.send(formData);
       };"]]))
