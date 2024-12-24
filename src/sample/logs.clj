5587108478153afb50485aebb39d0b1be0fe05d1
stankovicjana <stankovicjana000@gmail.com>
2024-12-24 14:14:38 +0100

    Updated monitoring to catch error

--- src/sample/logs.clj
+++ src/sample/logs.clj
@@ -1,10 +1,10 @@
 (ns sample.logs
-  (:import (java.nio.file Paths StandardWatchEventKinds WatchService)
-           (java.net URI)))
-
+  (:require [clojure.string :as s])
+  (:import [java.nio.file Paths StandardWatchEventKinds]
+           [java.net URI]))
 (defn watch-file
   []
-  (let [file-path (URI/create "file:///C:/Users/stank/OneDrive/Documents/User.txt")
+  (let [file-path (URI/create "file:///C:/new/test.txt")
         path (Paths/get file-path)
         parent-path (.getParent path)
         watch-service (.newWatchService (.getFileSystem path))]
@@ -15,10 +15,16 @@
         (let [key (.take watch-service)]
           (doseq [event (.pollEvents key)]
             (when (= (.kind event) StandardWatchEventKinds/ENTRY_MODIFY)
-              (let [updated-file (str (.resolve parent-path (.context event)))]
-                (println "File updated:" updated-file))))
+              (let [updated-file (str (.resolve parent-path (.context event)))
+                    content (slurp updated-file)]
+                (println "File updated:" updated-file)
+                (println "File content read:" content)
+                (when (some #(s/includes? (s/lower-case %) "error") (s/split content #"\n"))
+                  (println "Error detected in file! Updated content:")
+                  (println content)))))
           (.reset key)
           (recur))))))
+
 
 
 (watch-file)
